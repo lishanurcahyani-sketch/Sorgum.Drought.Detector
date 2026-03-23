e98df4f2aadaa9de109c2a6ffb87e7bce2d1ec71
lishanurcahyani-sketch <lishanurcahyani@gmail.com>
2026-03-24 01:25:47 +0700

    Add DroughtDetector class for Roboflow integration

---
+++ utils/roboflow_utils.py
@@ -0,0 +1,11 @@
+class DroughtDetector:
+    def __init__(self):
+        pass
+
+    def predict(self, data):
+        # Implement prediction logic here
+        pass
+
+    def detect_drought_severity(self, data):
+        # Implement drought severity detection logic here
+        pass
